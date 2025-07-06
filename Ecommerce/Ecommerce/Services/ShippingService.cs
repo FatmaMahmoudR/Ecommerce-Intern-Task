fb0e7ffef23184bc081decda24b89b2000c96251
Fatma Mahmoud <98098901+FatmaMahmoudR@users.noreply.github.com>
2025-07-07 01:25:40 +0300

    shipping service

---
+++ Ecommerce/Ecommerce/Services/ShippingService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Services
+{
+    internal class ShippingService
+    {
+        public decimal ShippingFee(double totalWeight) => (decimal)totalWeight * 2.5m;
+
+    }
+}
--- Ecommerce/Ecommerce/obj/Debug/net8.0/Ecommerce.AssemblyInfo.cs
+++ Ecommerce/Ecommerce/obj/Debug/net8.0/Ecommerce.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ced3a8fc6d4834ccc03d7458135324ebc8e6c63")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0f40c99f098c8f159f4616c30813bc3ea31fa10")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
